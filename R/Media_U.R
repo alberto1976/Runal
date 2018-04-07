882b307799cfaba66568f0b2fcb53349f43685f3
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2018-04-07 08:17:40 -0500

    Función promedio

---
+++ R/Media_U.R
@@ -0,0 +1,11 @@
+#' Una funcion que calcula un promedio unal
+#'
+#' @param vectorunal Un vector numerico
+#' @return El promedio unal calculado
+#' @export
+
+Media_U <- function(vectorunal){
+  vectorunal <- mean(vectorunal)*2
+  return(vectorunal)
+}
+
