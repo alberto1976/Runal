85780d8439087518cdadda1666fd05d4e31af982
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2018-04-07 08:26:17 -0500

    Se carga función que multiplica por 3

---
+++ R/Media_2.R
@@ -0,0 +1,10 @@
+#' Una funcion que calcula un promedio unal por 2
+#'
+#' @param vectorunal Un vector numerico
+#' @return El promedio unal calculado
+#' @export
+
+Media_2 <- function(vectorunal){
+  vectorunal <- mean(vectorunal)*3
+  return(vectorunal)
+}
